3fb85d7e393bf56953b7830c10beab1932a089ec
mpadge <mark.padgham@email.com>
2025-09-03 12:38:49 +0200

    improve structure of parking-areas

--- R/parking-areas.R
+++ R/parking-areas.R
@@ -1,9 +1,11 @@
 car_parking_areas <- function (osmdat, add_parking_osm_ids = NULL) {
 
+    lane_width <- 4
+
     hws <- reduce_osm_highways (osmdat$highways, osmdat$hw_names)
     hws$lanes [which (is.na (hws$lanes))] <- 1
     hws$lanes <- as.numeric (hws$lanes)
-    hws$road_area <- sf::st_length (hws) * units::set_units (hws$lanes * 4)
+    hws$road_area <- sf::st_length (hws) * units::set_units (hws$lanes * lane_width)
     hws <- parking_structure (hws)
 
     if (!is.null (add_parking_osm_ids)) {
@@ -17,6 +19,7 @@ car_parking_areas <- function (osmdat, add_parking_osm_ids = NULL) {
 }
 
 parking_structure <- function (hws) {
+
     parking <- sf::st_drop_geometry (hws [, grep ("parking", names (hws))])
     hws <- hws [, which (!grepl ("parking", names (hws)))]
 
@@ -25,6 +28,9 @@ parking_structure <- function (hws) {
     parking <- parking [, apply (parking, 2, function (i) any (!is.na (i)))]
 
     side_dir <- function (parking, side = "left") {
+
+        widths <- c (3, 4, 5) # (parallel, diagonal, perpendicular)
+
         parking_here <- parking [, grep (side, names (parking))]
         dirs <- c ("no|false", "parallel", "diagonal", "perpendicular")
         conditions <- lapply (dirs, function (d) {
@@ -37,12 +43,13 @@ parking_structure <- function (hws) {
         conditions <- do.call (cbind, conditions)
 
         parking_space <- rep (0, nrow (parking))
-        parking_space [which (conditions [, 2])] <- 3 # parallel
-        parking_space [which (conditions [, 3])] <- 4 # diagonal
-        parking_space [which (conditions [, 4])] <- 5 # perpendicular
+        for (i in 2:4) {
+            parking_space [which (conditions [, i])] <- widths [i = 1L]
+        }
 
         return (parking_space)
     }
+
     parking_sides <- side_dir (parking, "left") + side_dir (parking, "right")
     parking_both <- side_dir (parking, "both") * 2
     parking_space <- apply (cbind (parking_sides, parking_both), 1, max)
