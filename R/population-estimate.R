c25d20a953778074679e842f00c4223388a983e0
mpadge <mark.padgham@email.com>
2025-09-26 11:59:19 +0200

    add 'building_areas' fn fro #9

--- R/population-estimate.R
+++ R/population-estimate.R
@@ -15,7 +15,7 @@ area_per_resident <- function (osmdat) {
     i <- which (
         b$`addr:street` == street & b$`addr:housenumber` == housenumber
     )
-    area <- as.numeric (sf::st_area (osmdat$buildings [i, ])) # in m^2
+    area <- sf::st_area (osmdat$buildings [i, ]) # in m^2
 
     num_levels <- as.numeric (b$`building:levels` [i])
     num_roof_levels <- as.numeric (b$`roof:levels` [i])
@@ -27,4 +27,33 @@ area_per_resident <- function (osmdat) {
     a_per_res_roof <- 10 * floor (a_per_res_roof / 10)
 
     c (floor = a_per_res_floors, roof = a_per_res_roof)
+}
+
+building_areas <- function (osmdat) {
+
+    exclude <- c ("carport", "garage", "garages", "school", "shed", "yes")
+    b <- osmdat$buildings |>
+        dplyr::filter (!building %in% exclude) |>
+        dplyr::filter (!is.na (`addr:street`) & !is.na (`addr:housenumber`))
+
+    exclude_ground_floor <- c ("civic", "office", "retail", "supermarket")
+    num_levels <- as.numeric (b$`building:levels`)
+    index <- which (b$building %in% exclude_ground_floor & num_levels == 1)
+    if (length (index > 0)) {
+        b <- b [-index, ]
+        num_levels <- as.numeric (b$`building:levels`)
+        index <- which (b$building %in% exclude_ground_floor)
+    }
+    num_levels [index] <- num_levels [index] - 1
+    num_roof_levels <- as.numeric (b$`roof:levels`)
+
+    # Replace missing values with averages:
+    num_levels [which (is.na (num_levels))] <- mean (num_levels, na.rm = TRUE)
+    num_roof_levels [which (is.na (num_roof_levels))] <- mean (num_roof_levels, na.rm = TRUE)
+    areas <- sf::st_area (b)
+
+    area_levels <- areas * num_levels
+    area_roofs <- areas * num_roof_levels
+
+    c (floor = sum (area_levels), roof = sum (area_roofs))
 }
